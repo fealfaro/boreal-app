7a6f9f89bf3e1553a2de49e5ccbf4c145206f40f
Felipe <felipe@Felipes-MacBook-Air.local>
2026-04-25 23:40:28 -0400

    v1.21.1 force new bundle hash

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+      }
+    }
+  },
+  define: {
+    __BUILD_TIME__: JSON.stringify(new Date().toISOString()),
+  }
 })
